e8f9929903bc916a587de43c14f365ff272f586b
Puujee <puje.jj@gmail.com>
2013-09-07 20:01:43 +0800

    edit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by L on 9/7/13.
+//  Copyright (c) 2013 Sorako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubTest
+//
+//  Created by L on 9/7/13.
+//  Copyright (c) 2013 Sorako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
